22822bd4279b336bdde4cb85b837738331854b97
M-Aranda <cheloz_20@hotmail.com>
2020-03-04 17:37:16 -0300

    test

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -67,7 +67,7 @@ class StockPicking(models.Model):
 
     quality_weight = fields.Float('Kilos Calidad')
 
-    lol = fields.Char('Patente de camión', related='carrier_id.truck_patent')
+    lol = fields.Char('Patente de camión', related='carrier_id')
 
 #el problema tal vez este aqui
 #    carrier_truck_patent = fields.Char(
