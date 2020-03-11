703f606073b956e0a580be1a55eda7c7e0dd0203
M-Aranda <cheloz_20@hotmail.com>
2020-03-11 09:30:33 -0300

    test

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "My Library",  # Module title
-    'summary': "Manage books easily",  # Module subtitle phrase
-    'description': """Long description""",  # You can also rst format
-    'author': "Parth Gajjar",
-    'website': "http://www.example.com",
+    'name': "my_library",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
     'category': 'Uncategorized',
-    'version': '12.0.1',
-    'depends': ['base', 'website'],
-    # This data files will be loaded at the installation (commented becaues file is not added in this example)
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
     'data': [
-        'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/library_book.xml',
-        'views/templates.xml'
+        'views/views.xml',
+        'views/templates.xml',
     ],
- 
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
 }
