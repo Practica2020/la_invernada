8d354be4528252372ba023cf74c9928b935ff913
M-Aranda <cheloz_20@hotmail.com>
2020-03-11 11:12:14 -0300

    test

--- my_library/static/src/js/my_library.js
+++ my_library/static/src/js/my_library.js
@@ -1,7 +1,7 @@
 odoo.define('my_library', function (require) {
     var core = require('web.core');
 
-    alert(core._t('Hello world'));
+    alert(core._t('Su hola mundo piola'));
     return {
         // if you created functionality to export, add it here
     }
