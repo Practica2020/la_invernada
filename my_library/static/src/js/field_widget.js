5a53f0232ef3a7a02219ac811c7be22f23777712
M-Aranda <cheloz_20@hotmail.com>
2020-03-11 16:27:52 -0300

    test

--- my_library/static/src/js/field_widget.js
+++ my_library/static/src/js/field_widget.js
@@ -3,6 +3,8 @@ odoo.define('my_field_widget', function (require) {
 
 var AbstractField = require('web.AbstractField');
 var fieldRegistry = require('web.field_registry');
+var core = require('web.core');
+var qweb = core.qweb;
 
 var colorField = AbstractField.extend({
     className: 'o_int_colorpicker',
@@ -17,7 +19,9 @@ var colorField = AbstractField.extend({
     },
     _renderEdit: function () {
         this.$el.empty();
-        for (var i = 0; i < this.totalColors; i++ ) {
+        var pills = qweb.render('FieldColorPills', {widget : this });
+        this.$el.append(pills);
+       /* for (var i = 0; i < this.totalColors; i++ ) {
             var className = "o_color_pill o_color_" + i;
             if (this.value === i ) {
                 className += ' active';
@@ -26,7 +30,7 @@ var colorField = AbstractField.extend({
                 'class': className,
                 'data-val': i,
             }));
-        }
+         } */
     },
     _renderReadonly: function () {
         var className = "o_color_pill active readonly o_color_" + this.value;
