5d8e3a0135e7d87e7f391bc814e78dbf1f71efae
M-Aranda <cheloz_20@hotmail.com>
2020-03-12 14:14:48 -0300

    test

---
+++ farmacos/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+# class Farmacos(http.Controller):
+#     @http.route('/farmacos/farmacos/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/farmacos/farmacos/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('farmacos.listing', {
+#             'root': '/farmacos/farmacos',
+#             'objects': http.request.env['farmacos.farmacos'].search([]),
+#         })
+
+#     @http.route('/farmacos/farmacos/objects/<model("farmacos.farmacos"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('farmacos.object', {
+#             'object': obj
+#         })
