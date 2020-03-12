09dfab993f41fea548d09407ba991990ad143d05
M-Aranda <cheloz_20@hotmail.com>
2020-03-12 14:17:51 -0300

    test

--- farmacos/models/models.py
+++ farmacos/models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-# class farmacos(models.Model):
-#     _name = 'farmacos.farmacos'
+ class farmacos(models.Model):
+     _name = 'farmacos.farmacos'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
+
+     @api.depends('value')
+     def _value_pc(self):
+         self.value2 = float(self.value) / 100
