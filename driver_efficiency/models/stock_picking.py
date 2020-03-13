82473d57a20852f1e5e6cbae22c4a5ce523ed28f
M-Aranda <cheloz_20@hotmail.com>
2020-03-13 16:57:48 -0300

    test

---
+++ driver_efficiency/models/stock_picking.py
@@ -0,0 +1,19 @@
+from odoo import models, api, fields
+from datetime import datetime, timedelta
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+    _order = 'date desc'
+
+    
+     starting_date = fields.Date(
+         string='Inicio de actividades',
+         default=fields.Date.context_today,
+     )
+
+     
+     efficiency = fields.Float(
+         string='Eficacia',
+     )
+     
