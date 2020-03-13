cada285be96472b814122d7f5155043a9b59e852
M-Aranda <cheloz_20@hotmail.com>
2020-03-13 13:15:57 -0300

    test

---
+++ dimabe_coronavirus_tracking/models/medical_condition.py
@@ -0,0 +1,40 @@
+from odoo import models, fields, api
+
+class Medical_condition(models.Model):
+
+    _name = 'medical.condition'
+
+    dimabe_health_care_id = fields.Many2one(
+        string='Contacto de emergencia',
+        comodel_name='res.partner'
+    )
+
+    most_severe_disease = fields.Char('Condición más severa')
+
+    description = fields.Text('Descripción de la condición')
+
+    imagen = fields.Binary(string='imagen', attachment=True, help="for the lolz")
+    
+    certificado = fields.Binary(string='certificado', attachment=True)
+
+    
+    expiration_date = fields.Date(string='expiration_date', efault=fields.Date.context_today)
+    
+    
+    
+
+    @api.model
+    def create(self, values_list):
+        values_list = self._prepare_data(values_list)
+        return super(Medical_condition, self).create(values_list)
+
+    @api.multi
+    def write(self, vals):
+        vals = self._prepare_data(vals)
+        return super(Medical_condition, self).write(vals)
+
+
+
+
+  
+
